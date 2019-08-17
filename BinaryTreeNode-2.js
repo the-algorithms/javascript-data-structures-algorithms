ed92d03da92cfbc585f8f355da213771920c9d17
Parvez Kose <flamboyanz@gmail.com>
2019-08-16 23:26:44 -0400

    BST node 2

---
+++ BinaryTreeNode-2.js
@@ -0,0 +1,27 @@
+// Binary Tree Node ADT ////////////////////////////////////////////////
+
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insertLeft(value) {
+    this.left = new BinaryTreeNode(value);
+    return this.left;
+  }
+
+  insertRight(value) {
+    this.right = new BinaryTreeNode(value);
+    return this.right;
+  }
+}
+
+let treeRoot = new BinaryTreeNode(5);
+let leftNode = treeRoot.insertLeft(8);
+leftNode.insertLeft(1);
+leftNode.insertRight(2);
+let rightNode = treeRoot.insertRight(6);
+rightNode.insertLeft(3);
+rightNode.insertRight(4);
