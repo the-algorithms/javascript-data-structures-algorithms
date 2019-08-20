6c9d10ce40fd79b2461c3faa2c2d0dd648175a3a
Parvez Kose <flamboyanz@gmail.com>
2019-08-20 14:20:16 -0400

    Linked list node

---
+++ LinkedList.js
@@ -0,0 +1,37 @@
+// Linked List ADT ////////////////////////////////////////////////
+
+class LinkedListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+// method 1
+function appendToList(head, value) {
+  let tail = head;
+  while (tail.next) {
+    tail = tail.next;
+  }
+  tail.next = new LinkedListNode(value);
+  return tail.next;
+}
+
+let head = new LinkedListNode(1);
+let nodeToDelete = head;
+appendToList(head, 2);
+
+// method 2
+function valuesToLinkedListNodes(values) {
+  const nodes = [];
+  for (let i = 0; i < values.length; i++) {
+    const node = new LinkedListNode(values[i]);
+    if (i > 0) {
+      nodes[i - 1].next = node;
+    }
+    nodes.push(node);
+  }
+  return nodes;
+}
+
+let nodes = valuesToLinkedListNodes([1, 2, 3, 4]);
