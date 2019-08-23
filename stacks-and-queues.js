85ce3012b3266e076c724659a2f3ef2aaee150c5
Parvez Kose <flamboyanz@gmail.com>
2019-08-22 21:20:05 -0400

    stacks and queues ADT

---
+++ stacks-and-queues.js
@@ -0,0 +1,29 @@
+// Stack Class
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+  push(item) {
+    this.items.push(item);
+  }
+  pop() {
+    if (!this.items.length) return null;
+    return this.items.pop();
+  }
+}
+
+// Queue Class
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.size = 0;
+  }
+  enqueue(item) {
+    this.queue.unshift(item);
+    this.size += 1;
+  }
+  dequeue() {
+    this.size -= 1;
+    return this.queue.pop();
+  }
+}
