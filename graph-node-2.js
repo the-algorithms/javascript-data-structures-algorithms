1e6d51fc822d11dcfbff017400abbd6f8d47bf51
Parvez Kose <flamboyanz@gmail.com>
2019-09-11 15:14:56 -0400

    delete

--- graph-node-2.js
+++
@@ -1,9 +0,0 @@
-// Graph Node ADT ////////////////////////////////////////////////
-
-class GraphNode {
-  constructor(label) {
-    this.label = label;
-    this.neighbors = new Set();
-    this.color = null;
-  }
-}
