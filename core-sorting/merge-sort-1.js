f79ca9ac66f11686ebdd8bf8c3ad021de384a8da
Parvez Kose <flamboyanz@gmail.com>
2019-09-19 21:38:53 -0400

    merge sort update

--- core-sorting/merge-sort-1.js
+++ core-sorting/merge-sort-1.js
@@ -4,3 +4,29 @@
  * Space Complexity
  *
  */
+
+merge_sort;
+
+// Base case: single element list
+
+// Split the input in half
+
+// Sorted each half
+
+// merge the sorted list
+
+const combine_sorted_list = (list1, list2) => {
+  let list_one_index = 0;
+  let list_two_index = 0;
+  const merged_list = [];
+
+  // Both lists have some items left in them
+
+  // Choose the smaller of the two items and add it to the merged list
+
+  // Grab any lingering items (IF) in the first half after we have exhausted the second list
+
+  // Grab any lingering items (IF) in the second half after we have exhausted the first list
+
+  return merged_list;
+};
