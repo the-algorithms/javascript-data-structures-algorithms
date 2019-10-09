440b361ff83ac74c3884599f5a0f5105b63c7676
Parvez Kose <flamboyanz@gmail.com>
2019-10-09 13:09:57 -0400

    powere recursive

---
+++ algorithms/dynamic-programming/power-recur.js
@@ -0,0 +1,12 @@
+/*
+def power(base, expt):
+    # assume expt is non-negative integer
+    if (expt == 0):
+        return 1
+    elif (expt % 2 == 0):
+        return power(base, expt//2)**2
+    else:
+        return base * power(base, expt//2)**2
+
+print(power(2,5)) # 32
+*/
