587c29ddad6bf35c21c9af9833a1154e8dd43ef4
Parvez Kose <flamboyanz@gmail.com>
2019-12-16 23:36:36 -0500

    remove duplicates

---
+++ scratch/remove_duplicates.js
@@ -0,0 +1,30 @@
+var arr = [2, 1, 3, 2, 5, 6, 7, 2, 3];
+
+arr = arr.filter((num, index) => arr.indexOf(num) === index);
+arr.sort();
+console.log(arr);
+//
+
+var arr1 = [2, 1, 3, 2, 5, 6, 7, 2, 3];
+
+const res7 = arr => [...new Set(arr)].sort();
+console.log(res7(arr1));
+//
+
+var arr = [2, 1, 3, 2, 5, 6, 7, 3];
+var uniques = [];
+arr.forEach(num => {
+  if (!uniques.includes(num)) uniques.push(num);
+});
+console.log(uniques);
+//
+
+var arr1 = [2, 1, 3, 2, 5, 6, 7, 2, 3];
+
+const res21 = arr =>
+  arr.reduce(
+    (output, num) => (output.includes(num) ? output : [...output, num]),
+    []
+  );
+
+console.log(res21(arr1).sort());
