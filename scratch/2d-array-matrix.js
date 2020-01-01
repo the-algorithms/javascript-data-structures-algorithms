5d5f0de9f6ae311423b3cbb103bd3abb144f9030
Parvez Kose <flamboyanz@gmail.com>
2020-01-01 18:40:01 -0500

    scratch update

--- scratch/2d-array-matrix.js
+++ scratch/2d-array-matrix.js
@@ -1,4 +1,11 @@
 const dp = new Array(m).fill(null).map(() => ({ length: 1, count: 1 }));
 
+//
+// Init matrix with infinities meaning that there is no ways of
+    // getting from one vertex to another yet.
+    const adjacencyMatrix = Array(vertices.length).fill(null).map(() => {
+        return Array(vertices.length).fill(Infinity);
+
+//
 const visited = new Array(nr);
 for (var i = 0; i < visited.length; i++) visited[i] = new Array(nc).fill(false);
