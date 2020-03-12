2ed283adaae692e781bd21f4a7082e36f35ec335
Parvez Kose <flamboyanz@gmail.com>
2020-03-11 22:01:45 -0400

    quick sort

--- src/algorithms/sorting-new/quick-sort.js
+++ src/algorithms/sorting-new/quick-sort.js
@@ -6,31 +6,32 @@
  * 
  */
 
-const pivot = Math.floor(Math.random() * (right - left + 1) + left)
-   
-while (left <= right) {
-  while (array[left] < array[pivot])
-     left++;
-  while (array[right] > array[pivot])
-    right--;
+const partitionHoare = (array, left, right) => {
+  const pivot = Math.floor(Math.random() * (right - left + 1) + left)
+  
+  while (left <= right) {
+    while (array[left] < array[pivot])
+       left++;
+    while (array[right] > array[pivot])
+      right--;
 
-  if (left <= right) {
-    [array[left], array[right]] = [array[right], array[left]];
-    left++;
-    right--;
+    if (left <= right) {
+      [array[left], array[right]] = [array[right], array[left]];
+      left++;
+      right--;
+    }
   }
-}
-return left;
+  return left;
 }
 
 const quickSort = (array, left = 0, right = array.length - 1) => {
+ 
+  const pivot = partitionHoare(array, left, right);
 
-const pivot = partitionHoare(array, left, right);
+  if (left < pivot - 1) quickSort(array, left, pivot - 1);
+  if (right > pivot) quickSort(array, pivot, right);
 
-if (left < pivot - 1) quickSort(array, left, pivot - 1);
-if (right > pivot) quickSort(array, pivot, right);
-
-return array;
+  return array;
 }
 
 
